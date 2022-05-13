3d012ea3f4f27a513614d86373fa6605469ec97c
Marcel Boucher <mboucher@adobe.com>
2022-05-13 15:11:21 -0400

    ddom links added

--- blocks/ddom-nav/ddom-nav.js
+++ blocks/ddom-nav/ddom-nav.js
@@ -3,13 +3,18 @@ export default function decorate(block) {
     block.classList.add('phases-container');
     block.querySelectorAll(':scope > div').forEach((element,i) => {
             element.classList.add('phase-block');
-            
+            const link = document.createElement('a');
             const wrapper = document.createElement('div');
             wrapper.classList.add('phase-body');
             const items = element.querySelectorAll('div');
             items.forEach((item,i) => {
+                
                 if(i === 0) {
                     item.classList.add('phase-header', 'spectrum-Heading' ,'spectrum-Heading--sizeL');
+                    const itemText = item.innerText;
+                    link.href = item.querySelector('a').href;
+                    item.innerHTML = '';
+                    item.innerText = itemText;
                 }
                 if(i === 1) {
                     item.classList.add('icon');
@@ -19,9 +24,11 @@ export default function decorate(block) {
                 }
                 wrapper.appendChild(item);
             })
+
+            link.append(wrapper)
             
             addDivider(element);
-            element.appendChild(wrapper);
+            element.appendChild(link);
 
 
     });
